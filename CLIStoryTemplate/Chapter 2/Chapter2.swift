8c85dda0d2bfa7a5a356ed535dfad3b6a731221b
Arriel Jones <Jonesarriel11@gmail.com>
2023-11-06 10:32:30 -0500

    Chapter 2 update

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -4,7 +4,47 @@
 //
 
 import Foundation
+let title = "The Noises in The Basement"
+let LittleGirlName = "Amanda"
+var location = "Basement"
+var screechNoise = "errrrr"
+let beingInBasement = "\(LittleGirlName) was in the \(location) she heard a niose"
+var action = "She walked near the sound"
+let result = "saw a fast shadow run by"
+
+
 
 func chapterTwo() {
+    print(title)
+    print(beingInBasement)
+    print(action)
+    print(result)
+    decisionMaking()
     // Your portion of the story goes here
 }
+func decisionMaking() {
+    let result = decisionMaking(standStill: true, didntstandStill: false)
+    print(result)
+}
+func decisionMaking(standStill: Bool, didntstandStill: Bool) -> String {
+    if standStill {
+        return "She decided to stand still, in order to listen closely"
+    } else { 
+        return "She decided to make a run for it without looking back!"
+    }
+}
+
+enum standStill {
+    case Run
+    case Still
+    
+    
+    func StandStill(movement: String) {
+        if movement == "Run" {
+            print("running and screaming")
+        } else if movement == "Still" {
+            print("Breathing loud")
+        }
+    }
+}
+
